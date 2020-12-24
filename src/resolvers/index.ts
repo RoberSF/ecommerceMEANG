0d6212cf2b1ac81ea5b72e3aec65e3dfba27db97
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-12-24 12:36:58 +0100

    type para resolver dependencias entre tablas

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -6,9 +6,12 @@
 import { IResolvers } from 'graphql-tools';
 import query from './query';
 import mutation from './mutation';
+import type from './type';
+
 const resolvers: IResolvers = { // La inteerface de IResolvers ya viene en el paquete de Graphql
   ...query,
   ...mutation,
+  ...type
 };
 
 export default resolvers;
