7412d89027932793d77c2c5be2e5b4c4ab1cd0e9
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-04 13:02:14 +0100

    list query with pagination1

--- src/resolvers/query/stripe/customer.ts
+++ src/resolvers/query/stripe/customer.ts
@@ -6,23 +6,36 @@ import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 const resolversStripeCustomersQuery: IResolvers = {
     Query: {
 
-        async customers(_, { limit} ) {
+        async customers(_, { limit, startingAfter, endingBefore} ) {
 
+            let pagination;
+            if ( startingAfter !== '' && endingBefore === '') {
+                pagination = {
+                    starting_after: startingAfter
+                } 
+            } else if ( startingAfter === '' && endingBefore !== '' ) {
+                pagination = {
+                    ending_before: endingBefore
+                }
+                }
+                
             const stripe = new StripeApi().stripe;
             return await stripe.customers.list(
                 {
-                limit,
+                limit, ...pagination
                 }
               ).then( (result: {has_more: boolean, data: Array<IStripeCustomer>}) => {
                 return {
                     status: true,
                     message: `Lista cargada correctamente`,
+                    hasMore: result.has_more,
                     customers: result.data
                 };
               }).catch( (error: Error) => {
                 return {
                     status: false,
                     message: `Error:`.concat(error.message),
+                    hasMore: false,
                     customer: null
                 };
               });;
