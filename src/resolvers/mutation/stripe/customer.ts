98fd9683f5f2fb1f1cdcfa8c2bab94a62cf7a78d
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-04 20:20:44 +0100

    create user in stripe and update in mongo

--- src/resolvers/mutation/stripe/customer.ts
+++ src/resolvers/mutation/stripe/customer.ts
@@ -3,6 +3,9 @@ import slugify from 'slugify';
 import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 import StripeApi from '../../../lib/stripe.api';
 import { STRIPE_OBJECTS, STRIPE_ACTIONS } from '../../../lib/stripe.api';
+import { updateFindOne, updateOne, findOneElement } from '../../../lib/db-functions';
+import { COLLECTIONS } from '../../../config/constants';
+import { IUser } from '../../../interfaces/user.interface';
 
 
 const resolversStripeCustomerMutation: IResolvers = {
@@ -13,30 +16,68 @@ const resolversStripeCustomerMutation: IResolvers = {
     // genre = name
     async createCustomer(_, { name, email, description }, { db }) {
 
-      // Comprobamos que el cliente ya exista. (mismo email)
-
+      // 1º Comprobamos que el cliente ya exista en StripeDB. (mismo email)
       const userCheck: { data: Array<IStripeCustomer>} = await new StripeApi().execute(STRIPE_OBJECTS.CUSTOMERS, STRIPE_ACTIONS.LIST,{ email })
 
       if ( userCheck.data.length > 0 ) {
-        // Usuario existe
+        // 1.1.-Si el usuario existe:
         return {
           status: false,
           message: `El usuario con el email ${email} ya existe`
         }
       }
-
+        // 1.2.- Si el usuario no existe lo creamos en StripeDB y actualizamos MongoDB
+        //1.2.1.-Creamos usuario en StripeDB
         return await new StripeApi().execute(STRIPE_OBJECTS.CUSTOMERS, STRIPE_ACTIONS.CREATE,
           {
           name,
           email,
           description: `${name} (${email} )`           
           })
-          .then( (result: object) => {
-            return {
-                status: true,
-                message: `El cliente ${name} se ha creado correctamente`,
-                customer: result
-            };
+          // 1.2.1.1.- Una vez creado en StripeDB buscamos coincidencias en MongoDB y actualizamos
+          .then( async (result: IStripeCustomer) => {
+            const filterUserObject = { email: email};
+              // 1.2.1.1.1.- Primero buscamos el usuario en MongoDB
+               // Para que este await funcione, hay que poner el async en el result
+              const user: IUser =  await findOneElement(db,COLLECTIONS.USERS,filterUserObject)
+              console.log('1','He buscado a uno');
+              // 1.2.1.1.2.- Si existe usuario en MongoDB actualizamos
+              if(user) {
+                console.log('2','Existe el user y es',user);
+                // Este resul.id viene de la creación de stripe
+                user.stripeCustomer = result.id
+                const objectUpdate = { 
+                  stripeCustomer: user.stripeCustomer
+                 };
+                console.log('3', 'stripeCustomer es:',user.stripeCustomer);
+                const resultUserOperation = await updateOne(db,COLLECTIONS.USERS,filterUserObject,objectUpdate )
+                .then(
+                  result => {
+                    console.log('4','Después de actualizar uno:', result.result );
+                      // También hay result.n que nos dice el número de elementos que nos devolvió
+                      if (result) {
+                          return {
+                              status: true,
+                              message: `StripeId se ha añadido correctamente a db`,
+                              customer: userCheck.data
+                            };
+                      }
+                      return {
+                          status: false,
+                          message: `Error inesperado al añadir stripeid en db. Inténtalo de nuevo por favor.`,
+                          user: null
+                      }
+    
+                }) // Si no hay usuario en MongoDB tenemos que borrar el usuario creado de StripeDB con función específica
+          } 
+
+          return {
+            status: true,
+            message: `El cliente ${name} se ha creado correctamente`,
+            customer: result
+          }
+        
+          //1.2.1.1.- Si da error en crear usuario en StripeDB
           }).catch( (error: Error) => {
             return {
                 status: false,
@@ -51,3 +92,10 @@ const resolversStripeCustomerMutation: IResolvers = {
 
 
 export default resolversStripeCustomerMutation
+
+
+  // return {
+  //     status: true,
+  //     message: `El cliente ${name} se ha creado correctamente`,
+  //     customer: result
+  // };
