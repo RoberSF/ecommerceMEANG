9c2af92288dfa16e9a409c92c20227c7fe4656ea
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-05 09:55:19 +0100

    resolvers credit card

--- src/resolvers/mutation/stripe/index.ts
+++ src/resolvers/mutation/stripe/index.ts
@@ -1,9 +1,11 @@
 import GMR from 'graphql-merge-resolvers';
+import resolversStripeCardMutation from './card';
 import resolversStripeCustomerMutation from './customer';
 
 
 const stripeResolvers = GMR.merge([
     resolversStripeCustomerMutation,
+    resolversStripeCardMutation
 
 ]);
 
