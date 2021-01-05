be5581438c8640afcae7692b21c97c61a0887db7
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-05 15:44:38 +0100

    cardsPerCustomer function added

--- src/resolvers/query/stripe/card.ts
+++ src/resolvers/query/stripe/card.ts
@@ -27,6 +27,32 @@ const resolversStripeCreditCardQuery: IResolvers = {
           }
         })
     },
+
+    async cardsPerCustomer(_, { customer, limit, startingAfter, endingBefore} ) {
+
+      const pagination = new StripeApi().getPaginationOfStripe(startingAfter, endingBefore )
+
+            return await new StripeApi().execute(STRIPE_OBJECTS.CUSTOMERS, STRIPE_ACTIONS.LIST_CARDS,
+              customer,
+                {
+                    object: 'card', limit, ...pagination
+                })
+                .then( (result: {has_more: boolean, data: Array<IStripeCard>}) => {
+                return {
+                    status: true,
+                    message: `Lista cargada correctamente`,
+                    hasMore: result.has_more,
+                    cards: result.data
+                };
+              }).catch( (error: Error) => {
+                return {
+                    status: false,
+                    message: `Error:`.concat(error.message),
+                    hasMore: false,
+                    customer: null
+                };
+              });;
+    }
         
 
     }
