07f5eac714b2340d6c520b41e9a930fbf63924c7
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-05 09:55:45 +0100

    createCardMutation

---
+++ src/resolvers/mutation/stripe/card.ts
@@ -0,0 +1,44 @@
+import { IResolvers } from 'graphql-tools';
+import { createNamespaceExportDeclaration } from 'typescript';
+import StripeApi, { STRIPE_OBJECTS } from '../../../lib/stripe.api';
+import { STRIPE_ACTIONS } from '../../../lib/stripe.api';
+
+
+const resolversStripeCardMutation: IResolvers = {
+
+// Tipo raíz "Mutation"
+  Mutation: {
+    async createCardToken(_, {card}) {
+
+        return await new StripeApi().execute(
+          STRIPE_OBJECTS.TOKENS,
+          STRIPE_ACTIONS.CREATE,
+          {
+            card: {
+              number: card.number,
+              exp_month: card.expMonth,
+              exp_year: card.expYear,
+              cvc: card.cvc,
+            },
+          }
+        ).then ( (result: {id: string}) => {
+            console.log(result.id);
+            return {
+                status: true,
+                message: `Token ${result.id} creado correctamente`,
+                token: result.id
+            }
+        }).catch ( (error: Error) => {
+            console.log(card);
+            return {
+                status: false,
+                message: `Error: ${error}`,
+            }
+        })
+    }
+  }
+
+}
+
+
+export default resolversStripeCardMutation
