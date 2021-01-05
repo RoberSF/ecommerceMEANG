91febf75f93284db5ff1e276b8ce4a3b201003e8
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-05 20:31:59 +0100

    charge resolver added

--- src/resolvers/mutation/stripe/index.ts
+++ src/resolvers/mutation/stripe/index.ts
@@ -1,11 +1,13 @@
 import GMR from 'graphql-merge-resolvers';
 import resolversStripeCardMutation from './card';
+import resolversStripeChargeMutation from './charges';
 import resolversStripeCustomerMutation from './customer';
 
 
 const stripeResolvers = GMR.merge([
     resolversStripeCustomerMutation,
-    resolversStripeCardMutation
+    resolversStripeCardMutation,
+    resolversStripeChargeMutation
 
 ]);
 
