e13c23b43ebec7eb7387a77dfad415a9af86e198
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-09 00:59:39 +0100

    resolver charges added to index

--- src/resolvers/query/stripe/index.ts
+++ src/resolvers/query/stripe/index.ts
@@ -1,11 +1,13 @@
 import GMR from 'graphql-merge-resolvers';
 import resolversStripeCustomersQuery from './customer';
 import resolversStripeCreditCardQuery from './card';
+import resolversStripeChargesQuery from './charges';
 
 
 const queryStripeResolvers = GMR.merge([
     resolversStripeCustomersQuery,
-    resolversStripeCreditCardQuery
+    resolversStripeCreditCardQuery,
+    resolversStripeChargesQuery
 ]);
 
 export default queryStripeResolvers
