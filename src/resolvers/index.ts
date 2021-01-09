4282be2ff2d52a63d92fb4193dc58898cb5d5c79
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-09 17:17:52 +0100

    ...subscription added

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -7,11 +7,13 @@ import { IResolvers } from 'graphql-tools';
 import query from './query';
 import mutation from './mutation';
 import type from './type';
+import subscription from './suscription';
 
-const resolvers: IResolvers = { // La inteerface de IResolvers ya viene en el paquete de Graphql
+const resolvers: IResolvers = { // La interface de IResolvers ya viene en el paquete de Graphql
   ...query,
   ...mutation,
-  ...type
+  ...type,
+  ...subscription
 };
 
 export default resolvers;
